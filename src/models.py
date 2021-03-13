069a91071f20b7e7a191e9a5e88e2ca7c56b7e77
scottschmidl <sjschmidl@gmail.com>
2021-03-12 16:55:44 -0800

    working out dataframe for model

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 #place holder for machine learning models
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from life_well_main import clean_data
+from life_well_main import (clean_data, cleaned_groups, sort_mf_age, sort_mf, sort_age)
 import pandas as pd
 
 
@@ -13,11 +13,17 @@ import pandas as pd
 def main():
 
     df = pd.read_csv('data/wellbeing-lifestyle-cs1.csv')
-    well_life_df = clean_data(df)
 
-    print(well_life_df.head())
-    print(well_life_df.tail())
+    well_life_df = clean_data(df)
     print(well_life_df.info())
+
+    mf_age_group, mf_group, ages_group = cleaned_groups(df)
+
+    mf_ages_bal = sort_mf_age(mf, ages, m_f, mfa_group)
+    mf_bal = sort_mf(mf, m_f, mf_group)
+    bal_ages = sort_age(ages, ages_group)
+
+
 
 if __name__ == '__main__':
     main()
